e78fcbe74c1047f99e3145660022211d0b822989
waflawe <popejpivaoleg@gmail.com>
2025-02-26 05:16:06 +0900

    Add item model to admin

--- items/admin.py
+++ items/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+from items.models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    fields = "name", "description", "price"
+    list_display = "id", "name", "price"
+    list_display_links = "id", "name"
+    ordering = "-id", "-name"
+    list_per_page = 50
+    search_fields = "id", "name",
